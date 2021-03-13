6aebb3432e2856a76df0409451a87cafbf3f04ed
Petronellia <animalths@gmail.com>
2021-03-13 10:28:00 -0800

    add existing file

---
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -0,0 +1,50 @@
+package csc131.junit;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class GiftCardTest {
+
+	@Test
+	public void testGetIssuingStore() {
+		
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("getIssuingStore()",issuingStore, card.getIssuingStore());
+	}
+	@Test
+	public void getBalance() {
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		double tolerance;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		tolerance = 0.001;
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("getIssuingStore()", issuingStore, card.getIssuingStore(), tolerance);
+	}
+	@Test
+	public void deduct_RemainingBalance() {
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("getIssuingStore()", issuingStore, card.getIssuingStore());
+		//deduct();
+	
+	}
+
+}
